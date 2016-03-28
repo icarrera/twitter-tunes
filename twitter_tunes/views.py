0cc28875914c2fa59ca930b0fc70041e6d889ca5
Kyle R <KyleRichVA@gmail.com>
2016-03-28 16:57:29 -0700

    #19 created base sudocode for template and views.py

--- twitter_tunes/views.py
+++ twitter_tunes/views.py
@@ -4,4 +4,20 @@ from twitter_tunes.scripts import parser
 
 @view_config(route_name='home', renderer='templates/main_template.jinja2')
 def my_view(request):
-    return {'query': parser.parse_trend('#TwitterTunesRules')}
+    return {}
+
+
+def get_topics(request):
+    """return dict of twitter trends."""
+    # Make API request to twitter for top trends.
+    # return that.
+    pass
+
+
+def get_youtube_url(request):
+    """Return YT url of a specific trend."""
+    # Get trend out of ajax.
+    # parse the trend.
+    # get youtube url from the query.
+    # return that.
+    pass
