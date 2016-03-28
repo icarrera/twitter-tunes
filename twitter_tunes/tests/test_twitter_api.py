e61ceeb59f03fd8aa7914cea3b84be8f54e53b6c
icarrera <irismcarrera@gmail.com>
2016-03-28 16:13:56 -0700

    Asserted list output is string.

--- twitter_tunes/tests/test_twitter_api.py
+++ twitter_tunes/tests/test_twitter_api.py
@@ -1,6 +1,25 @@
-import mock
 
+# try:
+#     import unittest.mock
+# except:
+#     import mock
+
+import pytest
 # create_autospec
+from ..scripts.twitter_api import call_twitter_api
+
+FINAL_OUTPUT = ['trend1', 'trend2', 'trend3', 'trend4', 'trend5', 'trend6',
+                'trend7', 'trend8', 'trend9', 'trend10'
+                ]
+
 
 
 def test_twitter_okay():
+    pass
+
+
+# def test_final_output():
+#     assert len(call_twitter_api()) == len(FINAL_OUTPUT)
+
+def test_return_type():
+    assert isinstance(FINAL_OUTPUT[0], str)
