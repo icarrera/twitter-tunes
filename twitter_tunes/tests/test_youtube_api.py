869575d4c521a8fd93127a3bf439c4485ddfa89d
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-28 16:05:12 -0700

    created mock file for tests

--- twitter_tunes/tests/test_youtube_api.py
+++ twitter_tunes/tests/test_youtube_api.py
@@ -1,38 +1,33 @@
 # coding=utf-8
+import pytest
+import youtube_api
 
 
 # SOME OF THESE SEARCHES SHOULD BE ADDED TO CONFTEST TO REDUCE API CALLS #
 
-def test_youtube_search_get_data():
-    """Test to see if we are getting result from search."""
-    from youtube_api import youtube_search
-    keyword = 'test search'
-    result = youtube_search(keyword)
-    assert len(result) > 0
 
-
-def test_youtube_search_no_results():
-    """Test if you get empty item list from search with no results."""
-    from youtube_api import youtube_search
-    keyword = 'asdf safvdafvdsvafs'
-    result = youtube_search(keyword)
-    assert result.get('items') == []
-
-
-def test_youtube_search():
-    from youtube_api import youtube_search
+# def test_youtube_search_get_data():
+#     """Test to see if we are getting result from search."""
+#     keyword = 'test search'
+#     result = youtube_search(keyword)
+#     assert len(result) > 0
+#
+#
+# def test_youtube_search_no_results():
+#     """Test if you get empty item list from search with no results."""
+#     keyword = 'asdf safvdafvdsvafs'
+#     result = youtube_search(keyword)
+#     assert result.get('items') == []
 
 
 def test_youtube_parse_no_data():
     """Test that youtube search parser returns empty list with no data input"""
-    from youtube_api import youtube_parse
-    parsed = youtube_parse([])
+    parsed = youtube_api.youtube_parse([])
     assert parsed == []
 
 
 def test_generate_youtube_link_VEVO_priority():
     """Test link generator prioritizes VEVO links"""
-    from youtube_api import generate_youtube_link
     parsed_list = [('kTHNpusq654', 'CapitolMusic'),
                    ('tWbLkXhGEmo', 'CapitolMusic'),
                    ('wdGZBRAwW74', 'CapitolMusic'),
@@ -40,13 +35,12 @@ def test_generate_youtube_link_VEVO_priority():
                    ('47dtFZ8CFo8', 'CapitalCitiesVEVO'),
                    ('xopC0UndnYY', 'Vape Capitol'),
                    ('JqNGGsYoXt0', 'West Virginia Public Broadcasting')]
-    url = generate_youtube_link(parsed_list)
+    url = youtube_api.generate_youtube_link(parsed_list)
     assert url == 'https://www.youtube.com/watch?v=47dtFZ8CFo8'
 
 
 def test_generate_youtube_link_no_VEVO():
     """Test URL generator prioritizes first returned link"""
-    from youtube_api import generate_youtube_link
     parsed_list = [('oyEuk8j8imI', 'JustinBieberVEVO'),
                    ('fRh_vgS2dFE', 'JustinBieberVEVO'),
                    ('DK_0jXPuIr0', 'JustinBieberVEVO'),
@@ -56,12 +50,11 @@ def test_generate_youtube_link_no_VEVO():
                    ('2pvGCUoGXSc', 'Clevver News'),
                    ('Kn0YDZ3wifU', 'The Late Late Show with James Corden'),
                    ('Ca1i6DZC3iY', 'JustinBieberVEVO')]
-    url = generate_youtube_link(parsed_list)
+    url = youtube_api.generate_youtube_link(parsed_list)
     assert url == 'https://www.youtube.com/watch?v=oyEuk8j8imI'
 
 
 def test_generate_youtube_link_empty_list():
     """Test Lionel Richie returned if no results from search"""
-    from youtube_api import generate_youtube_link
-    url = generate_youtube_link([])
+    url = youtube_api.generate_youtube_link([])
     assert url == 'https://www.youtube.com/watch?v=b_ILDFp5DGA'
