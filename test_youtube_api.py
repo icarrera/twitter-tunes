28a612bf6b734d368e319820cc734a6d0459036a
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-28 10:28:31 -0700

    added error catch to youtube_search

---
+++ test_youtube_api.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+import pytest
+
+
+def test_youtube_search():
+    from youtube_api import youtube_search
+    
--- youtube_api.py
+++ youtube_api.py
@@ -8,5 +8,20 @@ DEVELOPER_KEY = os.environ.get('YT_AUTH')
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
-def youtube_search(keyword):
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey = DEVELOPER_KEY)
+
+def youtube_search(keyword, max_results=1):
+    try:
+        youtube = build(
+            YOUTUBE_API_SERVICE_NAME,
+            YOUTUBE_API_VERSION,
+            developerKey=DEVELOPER_KEY
+            )
+
+        search_response = youtube.search(
+        ).list(
+                q=keyword,
+                part='id,snippet',
+                maxResults=max_results).execute()
+        return search_response
+    except HttpError, e:
+        print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
