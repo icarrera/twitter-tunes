28db48f88b81aff32bea0d8dfe33865e2a6cdecc
Kyle R <KyleRichVA@gmail.com>
2016-03-28 12:00:59 -0700

    #12 Created parse_camel_case method and test.

--- twitter_tunes/scripts/parser.py
+++ twitter_tunes/scripts/parser.py
@@ -3,4 +3,13 @@ import re
 
 def parse_trend(trend):
     """Return a youtube search query based off a twitter trend."""
-    return trend.strip('#')
+    trend_no_tag = trend.strip('#')
+    # TODO: new function dealing with camel case
+    return trend_no_tag
+
+
+def parse_camel_trend(trend):
+    """Return string with spaces based off camelcase.
+
+    'MyHotTrend' would return 'My Hot Trend'
+    """
--- twitter_tunes/tests/test_scripts.py
+++ twitter_tunes/tests/test_scripts.py
@@ -1,10 +1,10 @@
 import pytest
 
 TEST_TRENDS = [
-    ('#EasterEggRoll', 'EasterEggRoll'),
-    ('#ArianaGrade', 'ArianaGrade'),
-    ('#mondaymotivation', 'mondaymotivation'),
-    ('Nick Swisher', 'Nick Swisher')
+    (u'#EasterEggRoll', u'EasterEggRoll'),
+    (u'#ArianaGrade', u'ArianaGrade'),
+    (u'#mondaymotivation', u'mondaymotivation'),
+    (u'Nick Swisher', u'Nick Swisher')
 ]
 
 
@@ -12,3 +12,8 @@ TEST_TRENDS = [
 def test_parse_trend(trend, query):
     from twitter_tunes.scripts import parser
     assert parser.parse_trend(trend) == query
+
+
+def test_camel_trend():
+    from twitter_tunes.scripts import parser
+    assert parser.parse_camel_trend(u'MyHotTrend') == u'My Hot Trend'
