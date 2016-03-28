41c1fde88054e129933809e302abb7abb5fcf75a
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-28 11:22:23 -0700

    added test for youtube generate url function

--- twitter_tunes/tests/test_youtube_api.py
+++ twitter_tunes/tests/test_youtube_api.py
@@ -13,3 +13,16 @@ def test_youtube_parse():
     from youtube_api import youtube_parse
     with pytest.raises(AttributeError):
         return youtube_parse([])
+
+
+def test_generate_youtube_link():
+    from youtube_api import generate_youtube_link
+    parsed_list = ['dJ8VjyPw0qY',
+                   '6ar-7XfCUMo',
+                   'jun4rWJ_L6I',
+                   'boGvieq9_HI',
+                   'E-1jee2laSM',
+                   'cVi25pSRAkc',
+                   'y2ak_oBeC-I']
+    url = generate_youtube_link(parsed_list)
+    assert url == 'https://www.youtube.com/watch?v=dJ8VjyPw0qY'
