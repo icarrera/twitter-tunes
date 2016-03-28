e016285188690c4cfede68e3663ec098d4b74c35
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-28 10:49:30 -0700

    edited yt search test

--- twitter_tunes/tests/test_youtube_api.py
+++ twitter_tunes/tests/test_youtube_api.py
@@ -4,4 +4,6 @@ import pytest
 
 def test_youtube_search():
     from youtube_api import youtube_search
-    
+    keyword = 'test search'
+    result = youtube_search
+    assert 'videoId' in result
--- youtube_api.py
+++ youtube_api.py
@@ -4,17 +4,17 @@ from apiclient.errors import HttpError
 import os
 
 
-DEVELOPER_KEY = os.environ.get('YT_AUTH')
+YOUTUBE_DEVELOPER_KEY = os.environ.get('YT_AUTH')
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
 
-def youtube_search(keyword, max_results=1):
+def youtube_search(keyword, max_results=10):
     try:
         youtube = build(
             YOUTUBE_API_SERVICE_NAME,
             YOUTUBE_API_VERSION,
-            developerKey=DEVELOPER_KEY
+            developerKey=YOUTUBE_DEVELOPER_KEY
             )
 
         search_response = youtube.search(
@@ -23,5 +23,5 @@ def youtube_search(keyword, max_results=1):
                 part='id,snippet',
                 maxResults=max_results).execute()
         return search_response
-    except HttpError, e:
-        print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+    except HttpError:
+        print('An HTTP error has occurred.')
