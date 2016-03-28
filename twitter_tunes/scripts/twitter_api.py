8827210a8ab1a55c5c032df40677a5343818ac91
Ben Garnaat <garnaat.ben@gmail.com>
2016-03-28 12:38:53 -0700

    twitter API call / tweepy functional

--- twitter_tunes/scripts/twitter_api.py
+++ twitter_tunes/scripts/twitter_api.py
@@ -3,10 +3,8 @@
 import tweepy
 import os
 
-REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
-AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
-ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
 
+WOEID_US = 23424977
 consumerKey = os.environ.get('TWITTER_CONSUMERKEY')
 consumerSecret = os.environ.get('TWITTER_CONSUMERSECRET')
 accessToken = os.environ.get('TWITTER_ACCESSTOKEN')
@@ -16,4 +14,4 @@ auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
-print(api.trends_place(1))
+print(api.trends_place(WOEID_US))
