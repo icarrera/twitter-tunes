f0efba845c0132d9eb2d02d1e307d11332d93bf4
Kyle R <KyleRichVA@gmail.com>
2016-03-28 16:26:34 -0700

    Added 'test' to see if pyramid view can run script function.

--- twitter_tunes/views.py
+++ twitter_tunes/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
+from twitter_tunes.scripts import parser
 
 
 @view_config(route_name='home', renderer='templates/main_template.jinja2')
 def my_view(request):
-    return {}
+    return {'query': parser.parse_trend('#TwitterTunesRules')}
