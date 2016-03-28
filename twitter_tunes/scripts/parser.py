12ebd9a683a3a7e3f7d005ea7c29be4873a33fe4
Kyle R <KyleRichVA@gmail.com>
2016-03-28 14:28:21 -0700

    #12 Added comments for parse camel case

--- twitter_tunes/scripts/parser.py
+++ twitter_tunes/scripts/parser.py
@@ -14,12 +14,16 @@ def parse_camel_trend(trend):
     'MyHotTrend' would return 'My Hot Trend'
     'NBAFinals' would return 'NBA Finals'
     'MTV' would return 'MTV'
+    'UFC200 would return UFC 200'
     """
+    # Split the trend into words based off regex groups.
     camel_re = re.compile(r'''
-        (\W+|
-        \d+|
+        (\W+|  # One or more non-word characters
+        \d+|  # One or more digit characters
+        # 1+ uppercase followed by 1+ uppercase and 1+ lowercases
         [A-Z]+(?=[A-Z]+[a-z]+)|
-        [A-Z]+[a-z]*)
+        [A-Z]+[a-z]*)  # 1+ uppercase followed by any number of lowercases
         ''', re.VERBOSE)
     words = re.findall(camel_re, trend)
+    # Combine the words into a single string seperated by spaces.
     return u' '.join(words)
