a0c54d869cf55b5c1a52d4efba8ac400e523ee69
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-28 16:07:30 -0700

    specified unicode in tests

--- twitter_tunes/tests/test_youtube_api.py
+++ twitter_tunes/tests/test_youtube_api.py
@@ -28,28 +28,28 @@ def test_youtube_parse_no_data():
 
 def test_generate_youtube_link_VEVO_priority():
     """Test link generator prioritizes VEVO links"""
-    parsed_list = [('kTHNpusq654', 'CapitolMusic'),
-                   ('tWbLkXhGEmo', 'CapitolMusic'),
-                   ('wdGZBRAwW74', 'CapitolMusic'),
-                   ('1-pUaogoX5o', 'emimusic'),
-                   ('47dtFZ8CFo8', 'CapitalCitiesVEVO'),
-                   ('xopC0UndnYY', 'Vape Capitol'),
-                   ('JqNGGsYoXt0', 'West Virginia Public Broadcasting')]
+    parsed_list = [(u'kTHNpusq654', u'CapitolMusic'),
+                   (u'tWbLkXhGEmo', u'CapitolMusic'),
+                   (u'wdGZBRAwW74', u'CapitolMusic'),
+                   (u'1-pUaogoX5o', u'emimusic'),
+                   (u'47dtFZ8CFo8', u'CapitalCitiesVEVO'),
+                   (u'xopC0UndnYY', u'Vape Capitol'),
+                   (u'JqNGGsYoXt0', u'West Virginia Public Broadcasting')]
     url = youtube_api.generate_youtube_link(parsed_list)
     assert url == 'https://www.youtube.com/watch?v=47dtFZ8CFo8'
 
 
 def test_generate_youtube_link_no_VEVO():
     """Test URL generator prioritizes first returned link"""
-    parsed_list = [('oyEuk8j8imI', 'JustinBieberVEVO'),
-                   ('fRh_vgS2dFE', 'JustinBieberVEVO'),
-                   ('DK_0jXPuIr0', 'JustinBieberVEVO'),
-                   ('PfGaX8G0f2E', 'JustinBieberVEVO'),
-                   ('ztWFp63QPj4', 'The Late Late Show with James Corden'),
-                   ('djzDWMy1z7k', 'JustinBieberVEVO'),
-                   ('2pvGCUoGXSc', 'Clevver News'),
-                   ('Kn0YDZ3wifU', 'The Late Late Show with James Corden'),
-                   ('Ca1i6DZC3iY', 'JustinBieberVEVO')]
+    parsed_list = [(u'oyEuk8j8imI', u'JustinBieberVEVO'),
+                   (u'fRh_vgS2dFE', u'JustinBieberVEVO'),
+                   (u'DK_0jXPuIr0', u'JustinBieberVEVO'),
+                   (u'PfGaX8G0f2E', u'JustinBieberVEVO'),
+                   (u'ztWFp63QPj4', u'The Late Late Show with James Corden'),
+                   (u'djzDWMy1z7k', u'JustinBieberVEVO'),
+                   (u'2pvGCUoGXSc', u'Clevver News'),
+                   (u'Kn0YDZ3wifU', u'The Late Late Show with James Corden'),
+                   (u'Ca1i6DZC3iY', u'JustinBieberVEVO')]
     url = youtube_api.generate_youtube_link(parsed_list)
     assert url == 'https://www.youtube.com/watch?v=oyEuk8j8imI'
 
