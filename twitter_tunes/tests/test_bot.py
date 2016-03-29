65bbecbd0757066b888d5feeecd7f08e5084b563
Kyle R <KyleRichVA@gmail.com>
2016-03-29 11:35:40 -0700

    #31 creates right message when using other functions for params.

--- twitter_tunes/tests/test_bot.py
+++ twitter_tunes/tests/test_bot.py
@@ -11,3 +11,21 @@ def test_bot_create_message_known_params():
     url = u'https://www.youtube.com/watch?v=rTfa-9aCTYg'
     message = twitter_bot.create_message(u'A Trend', url)
     assert u'A Trend' in message and url in message
+
+
+def test_bot_function_params():
+    """Test to see bot can return a message.
+
+    Message should be based on the returns of other functions.
+    """
+    from twitter_tunes.scripts import parser, youtube_api
+    trend = u"#StoryFromNorthAmerica"
+    parse_trend = parser.parse_trend(trend)
+    url = youtube_api.generate_youtube_link(
+        youtube_api.youtube_parse(
+            youtube_api.youtube_search(parse_trend)
+            )
+        )
+    message = twitter_bot.create_message(parse_trend, url)
+    assert (u'Story From North America' in message and
+            u'https://www.youtube.com/watch?v=ms2klX-puUU' in message)
