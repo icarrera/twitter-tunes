f5ca2368e0a49f07a82e54ca3e1b95ada2d92499
Ben Garnaat <garnaat.ben@gmail.com>
2016-03-29 11:20:45 -0700

    renamed index.html index.jinja2 added api call to view. added hardcoded test data due to rate limit exeeded

--- twitter_tunes/views.py
+++ twitter_tunes/views.py
@@ -1,10 +1,12 @@
 from pyramid.view import view_config
-from twitter_tunes.scripts import parser
+from twitter_tunes.scripts import parser, twitter_api
 
 
-@view_config(route_name='home', renderer='templates/main_template.jinja2')
+@view_config(route_name='home', renderer='templates/index.jinja2')
 def my_view(request):
-    return {}
+    # trends = twitter_api.call_twitter_api()
+    trends = ['one', 'two']
+    return {'trends': trends}
 
 
 def get_topics(request):
