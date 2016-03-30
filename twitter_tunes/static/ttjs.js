163c730377fd24632000ccaa23521aaa69b96d9d
wohlfea <wohlfea@gmail.com>
2016-03-29 18:02:53 -0700

    able to check if the source is already loaded when section is clicked on

--- twitter_tunes/static/ttjs.js
+++ twitter_tunes/static/ttjs.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
-    update($('#trends > ul > li:first'));
+    update_first($('#trends > ul > li:first'));
     $('#trends').on('click', 'li', function(){
         update($(this))
     })
 });
 
+function check_loaded(iframe){
+    if(iframe.attr('src') == '../static/loading.gif'){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function update(trend){
-    console.log(trend.attr('id'))
+    iframe = trend.children('article').children('iframe')
+    if(check_loaded(iframe)){
+        console.log('loading...')
+        $.get('/youtube/' + trend.attr('id'), function(data){
+            iframe.attr('src', data.url)
+        })
+    } else {
+        console.log('src is loaded')
+    }
+}
+
+function update_first(trend){
+    iframe = trend.children('article').children('iframe')
     $.get('/youtube/' + trend.attr('id'), function(data){
-        trend.children('article').children('iframe').attr('src', data.url)
+        iframe.attr('src', data.url)
     })
 }
