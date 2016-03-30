3dedc51d24c451f931b8c53ec17baa0f65587429
wohlfea <wohlfea@gmail.com>
2016-03-30 12:26:16 -0700

    scheduler python file added.

---
+++ clock.py
@@ -0,0 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from twitter_tunes.scripts import twitter_bot
+
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=2)
+def timed_job():
+    twitter_bot()
+
+sched.start()
