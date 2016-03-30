1335cdc0a94312ed3f9843129173fdc3489cbd63
Kyle R <KyleRichVA@gmail.com>
2016-03-30 11:48:05 -0700

    Changed over main test to use all mock data and changed twitter_api test data to be unicode strs.

--- twitter_tunes/tests/test_bot.py
+++ twitter_tunes/tests/test_bot.py
@@ -14,28 +14,32 @@ def test_make_tweet_static_message(api):
     mock_method.assert_called_with(u"more tests")
 
 
+@mock.patch('twitter_tunes.scripts.twitter_api')
+@mock.patch('twitter_tunes.scripts.youtube_api')
 @mock.patch('tweepy.API')
-def test_make_tweet_main(api):
+def test_make_tweet_main_good(api, twitter_api, youtube_api):
     """Test if bot makes tweet with api data.
 
     This is what the main function would do.
     """
-    from twitter_tunes.scripts import parser, youtube_api
-    mock_method = api().update_status
-    import twitter_tunes.tests.bot_test_vars as bot_test_vars
-    top_trend = twitter_bot.choose_trend(bot_test_vars.TRENDS)
+    from twitter_tunes.scripts import parser, youtube_api, twitter_api
+    from twitter_tunes.tests import test_twitter_api
+
+    mock_update_status = api().update_status
+    mock_trends = twitter_api()
+    mock_yt_search = youtube_api()
+    mock_trends.call_twitter_api.return_value = test_twitter_api.FINAL_OUTPUT
+    mock_yt_search.get_link.return_value = u"https://www.youtube.com/watch?v=oyEuk8j8imI"
+
+    trends = mock_trends.call_twitter_api()
+    top_trend = twitter_bot.choose_trend(trends)
     parse_trend = parser.parse_trend(top_trend)
     # Results that would come from searching this trend.
     # Saved locally to prevent api calls each test.
-    search_results = bot_test_vars.N_A_SEARCH_RESULTS
-    url = youtube_api.generate_youtube_link(
-        youtube_api.youtube_parse(
-                search_results
-            )
-        )
+    url = mock_yt_search.get_link(parse_trend)
     message = twitter_bot.create_message(parse_trend, url)
     twitter_bot.make_tweet(message)
-    mock_method.assert_called_with(message)
+    mock_update_status.assert_called_with(message)
 
 
 def test_bot_create_message_known_params():
