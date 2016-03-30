079423394c139c96a11c09028ebf171216b731ea
wohlfea <wohlfea@gmail.com>
2016-03-29 18:21:05 -0700

    views are loading appropriately upon respective clicks. Loading does not occur if either video is already loaded.

--- twitter_tunes/static/ttjs.js
+++ twitter_tunes/static/ttjs.js
@@ -1,33 +1,39 @@
 $(document).ready(function() {
-    update_first($('#trends > ul > li:first'));
+    update_one($('#trends > ul > li:first'));
     $('#trends').on('click', 'li', function(){
-        update($(this))
+        update($(this));
     })
 });
 
 function check_loaded(iframe){
     if(iframe.attr('src') == '../static/loading.gif'){
         return true;
-    } else {
-        return false;
+    }
+    return false;
+}
+
+function update_next(cur_trend){
+    next_trend_iframe = cur_trend.next().children('article').children('iframe');
+    if(check_loaded(next_trend_iframe)){
+        update_one(cur_trend.next());
     }
 }
 
 function update(trend){
     iframe = trend.children('article').children('iframe')
     if(check_loaded(iframe)){
-        console.log('loading...')
         $.get('/youtube/' + trend.attr('id'), function(data){
-            iframe.attr('src', data.url)
+            iframe.attr('src', data.url);
+            update_next(trend);
         })
     } else {
-        console.log('src is loaded')
+        update_next(trend);
     }
 }
 
-function update_first(trend){
-    iframe = trend.children('article').children('iframe')
+function update_one(trend){
+    iframe = trend.children('article').children('iframe');
     $.get('/youtube/' + trend.attr('id'), function(data){
-        iframe.attr('src', data.url)
+        iframe.attr('src', data.url);
     })
 }
