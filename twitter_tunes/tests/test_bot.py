7e22a69d534d2b3955f0bfcbd47b1f7fd82a47fc
Kyle R <KyleRichVA@gmail.com>
2016-03-31 15:59:06 -0700

    #101 covered long last tweet list.

--- twitter_tunes/tests/test_bot.py
+++ twitter_tunes/tests/test_bot.py
@@ -193,5 +193,23 @@ def test_bot_choose_trend_bad_redis(get_link, get_redis_data, set_redis_data):
     get_redis_data.side_effect = redis_side_effect_bad
     redis_trends = get_redis_data(u'trends')
     trends = redis_trends[u'trends']
-    import pdb; pdb.set_trace()
+    assert twitter_bot.choose_trend(trends)[0] == bot_test_vars.TRENDS[0]
+
+
+@mock.patch('twitter_tunes.scripts.twitter_bot.redis_data.set_redis_data')
+@mock.patch('twitter_tunes.scripts.twitter_bot.redis_data.get_redis_data')
+@mock.patch('twitter_tunes.scripts.twitter_bot.youtube_api.get_link')
+def test_bot_choose_trend_trend_long(get_link, get_redis_data, set_redis_data):
+    """test if the choosing trend handles a long last_tweets"""
+    get_link.return_value = (u'url', True)
+
+    def redis_side_effect_bad(arg):
+        if arg == u'trends':
+            return bot_test_vars.REDIS_TRENDS
+        elif arg == u'last_tweets':
+            return [u'trend', u'trend', u'trend', u'trend', u'trend']
+
+    get_redis_data.side_effect = redis_side_effect_bad
+    redis_trends = get_redis_data(u'trends')
+    trends = redis_trends[u'trends']
     assert twitter_bot.choose_trend(trends)[0] == bot_test_vars.TRENDS[0]
