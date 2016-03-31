0d2af3751f9c5c850e0e2f8d43b88a8aa84ab9f7
wohlfea <wohlfea@gmail.com>
2016-03-30 17:28:15 -0700

    commenting out redis scheduler to see if bot will run properly without the 2nd schedule.

--- clock.py
+++ clock.py
@@ -13,10 +13,10 @@ def timed_job():
     twitter_bot.main()
 
 
-@sched.scheduled_job('interval', minutes=9)
-def timed_job_redis():
-    trend_list = twitter_api.call_twitter_api()
-    redis_data.set_redis_trend_list(trend_list)
+# @sched.scheduled_job('interval', minutes=9)
+# def timed_job_redis():
+#     trend_list = twitter_api.call_twitter_api()
+#     redis_data.set_redis_trend_list(trend_list)
 
 
 sched.start()
